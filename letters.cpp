d175f4903c91901c27f84e56c0e8815b49fb1e83
silviangulica <gulica.sv@gmail.com>
2021-10-21 01:26:13 +0300

    Versiunea 0.24

--- letters.cpp
+++ letters.cpp
@@ -1,14 +1,3 @@
-/*
-    Creator: 3pletii 
-    Versiuni:
-        0.24 -> Adaugat Github, yey!
-        0.23 -> Optimizari vizuale la cod si la menu
-	    0.22 -> Bug, doua nu doi pt 572
-        0.21 -> Modificari pentru "200", gen douasunte
-        0.2 -> Functioneaza pentru numerele pana la 1000;
-        0.1 -> Lucreaza doar pana la nr 100;
-
-*/
 #include <iostream>
 using namespace std;
 
--- readme.md
+++ readme.md
@@ -1,1 +1,12 @@
-# Learnin GitHub
+# Proiect: Convertirea numerelor in cuvinte
+# Versiunea: 0.24
+
+    -Changelog-
+
+    0.24 -> Integrare Github, yey!
+    0.23 -> Optimizari vizuale la cod si la menu
+	0.22 -> Bug, doua nu doi pt 572
+    0.21 -> Modificari pentru "200", gen douasunte
+    0.2 -> Functioneaza pentru numerele pana la 1000;
+    0.1 -> Lucreaza doar pana la nr 100;
+
